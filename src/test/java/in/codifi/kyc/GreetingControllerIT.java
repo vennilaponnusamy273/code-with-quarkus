b8f0734d1a1bd0d7fe6afd950292b947d5a5134f
quarkusio <no-reply@quarkus.io>
2024-06-28 05:16:13 +0000

    Initial commit

---
+++ src/test/java/in/codifi/kyc/GreetingControllerIT.java
@@ -0,0 +1,8 @@
+package in.codifi.kyc;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingControllerIT extends GreetingControllerTest {
+    // Execute the same tests but in packaged mode.
+}
